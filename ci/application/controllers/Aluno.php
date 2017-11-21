ae2fd6adb3a0a76b2a21f4f46a55f34ff5d1f672
Juciara Lima <juciara.slima@gmail.com>
2017-11-21 13:35:02 +0000

    começando o cadastro de aluno e login

--- ci/application/controllers/Aluno.php
+++ ci/application/controllers/Aluno.php
@@ -7,4 +7,20 @@ class Aluno extends CI_Controller {
 	{
 		$this->load->view('feed');
 	}
+	
+	public function cadastro(){
+		$nome = $this->input->post("nome");
+		$email = $this->input->post("email");
+		$senha = $this->input->post("senha");
+		$ra = $this->input->post("ra");
+		$curso = $this->input->post("curso");
+		require_once APPPATH."models/aluno.php";
+		$alu = new AlunoModel(0,$nome,$email,$senha,$ra,$curso);
+		//NAO HA CONSTRUTOR NOS DAO'S
+		$this->load->model('insertdao');
+		$insdao = $this->insertdao;
+		$insdao->insert($alu);
+		$this->session->set_userdata("msg","Usuário Cadastrado");
+		redirect('/login/form','refresh');
+	}
 }
