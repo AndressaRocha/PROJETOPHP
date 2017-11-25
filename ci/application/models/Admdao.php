ee105e90876f83ff98e3bf16ab27cafae27ba68c
Juciara Lima <juciara.slima@gmail.com>
2017-11-25 16:15:13 +0000

    inicializando as funcionalidades do adm

---
+++ ci/application/models/Admdao.php
@@ -0,0 +1,26 @@
+<?php
+    class AlunoDAO extends CI_Model{
+        
+        public function getUser($email,$senha){
+            //SELECT * FROM Usuario WHERE
+            //email = $email AND senha = $senha 
+            $this->db->where('email',$email);
+		    $this->db->where('senha',$senha);
+		    $adm = $this->db->get('adm');
+		    require_once APPPATH."models/adm.php";
+		    if ($adm->num_rows()>0){
+		        //SE FOSSEM VARIOS, FOR
+		        $administrador = $adm->result()[0];
+		        $id = $administrador->id;
+		        $nome = $administrador->nome;
+		        $emailAlu = $administrador->email;
+		        $senhaAlu = $administrador->senha;
+		        return new AdmModel($id,$nome,$emailAlu,$senhaAlu);
+            }else{
+                return null;
+            }
+    
+        }
+    }
+    
+?>
